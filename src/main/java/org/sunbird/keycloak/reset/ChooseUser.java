ad6cfccdf3e777ee9726d865be454b308d84ce65
indrajra-ilimi <rajeshr@ilimi.in>
2020-04-01 11:58:02 +0530

    Add choose user to reset-password

---
+++ src/main/java/org/sunbird/keycloak/reset/ChooseUser.java
@@ -0,0 +1,186 @@
+package org.sunbird.keycloak.reset;
+
+
+import org.jboss.logging.Logger;
+import org.keycloak.Config;
+import org.keycloak.authentication.AuthenticationFlowContext;
+import org.keycloak.authentication.AuthenticationFlowError;
+import org.keycloak.authentication.Authenticator;
+import org.keycloak.authentication.AuthenticatorFactory;
+import org.keycloak.authentication.actiontoken.DefaultActionTokenKey;
+import org.keycloak.authentication.authenticators.broker.AbstractIdpAuthenticator;
+import org.keycloak.authentication.authenticators.browser.AbstractUsernameFormAuthenticator;
+import org.keycloak.events.Details;
+import org.keycloak.events.Errors;
+import org.keycloak.events.EventBuilder;
+import org.keycloak.models.AuthenticationExecutionModel;
+import org.keycloak.models.KeycloakSession;
+import org.keycloak.models.KeycloakSessionFactory;
+import org.keycloak.models.RealmModel;
+import org.keycloak.models.UserModel;
+import org.keycloak.provider.ProviderConfigProperty;
+import org.keycloak.services.messages.Messages;
+import org.sunbird.keycloak.core.CustomUser;
+
+import javax.ws.rs.core.MultivaluedMap;
+import javax.ws.rs.core.Response;
+import java.util.List;
+
+
+public class ChooseUser implements Authenticator, AuthenticatorFactory {
+
+    private static final Logger logger = Logger.getLogger(ChooseUser.class);
+
+    public static final String PROVIDER_ID = "spi-reset-credentials-choose-user";
+
+    @Override
+    public void authenticate(AuthenticationFlowContext context) {
+        String existingUserId = context.getAuthenticationSession().getAuthNote(AbstractIdpAuthenticator.EXISTING_USER_INFO);
+        if (existingUserId != null) {
+            UserModel existingUser = AbstractIdpAuthenticator.getExistingUser(context.getSession(), context.getRealm(), context.getAuthenticationSession());
+
+            logger.debugf("Forget-password triggered when reauthenticating user after first broker login. Skipping reset-credential-choose-user screen and using user '%s' ", existingUser.getUsername());
+            context.setUser(existingUser);
+            context.success();
+            return;
+        }
+
+        String actionTokenUserId = context.getAuthenticationSession().getAuthNote(DefaultActionTokenKey.ACTION_TOKEN_USER_ID);
+        if (actionTokenUserId != null) {
+            UserModel existingUser = context.getSession().users().getUserById(actionTokenUserId, context.getRealm());
+
+            // Action token logics handles checks for user ID validity and user being enabled
+
+            logger.debugf("Forget-password triggered when reauthenticating user after authentication via action token. Skipping reset-credential-choose-user screen and using user '%s' ", existingUser.getUsername());
+            context.setUser(existingUser);
+            context.success();
+            return;
+        }
+
+        Response challenge = context.form().createPasswordReset();
+        context.challenge(challenge);
+    }
+
+    @Override
+    public void action(AuthenticationFlowContext context) {
+        EventBuilder event = context.getEvent();
+        MultivaluedMap<String, String> formData = context.getHttpRequest().getDecodedFormParameters();
+        String username = formData.getFirst("username");
+        if (username == null || username.isEmpty()) {
+            event.error(Errors.USERNAME_MISSING);
+            Response challenge = context.form()
+                    .setError(Messages.MISSING_USERNAME)
+                    .createPasswordReset();
+            context.failureChallenge(AuthenticationFlowError.INVALID_USER, challenge);
+            return;
+        }
+
+        username = username.trim();
+
+        RealmModel realm = context.getRealm();
+        UserModel user = new CustomUser().findUserByNameOrEmail(context.getSession(), realm, username);
+        if (user == null && realm.isLoginWithEmailAllowed() && username.contains("@")) {
+            user = context.getSession().users().getUserByEmail(username, realm);
+        }
+
+        context.getAuthenticationSession().setAuthNote(AbstractUsernameFormAuthenticator.ATTEMPTED_USERNAME, username);
+
+        // we don't want people guessing usernames, so if there is a problem, just continue, but don't set the user
+        // a null user will notify further executions, that this was a failure.
+        if (user == null) {
+            event.clone()
+                    .detail(Details.USERNAME, username)
+                    .error(Errors.USER_NOT_FOUND);
+            context.clearUser();
+        } else if (!user.isEnabled()) {
+            event.clone()
+                    .detail(Details.USERNAME, username)
+                    .user(user).error(Errors.USER_DISABLED);
+            context.clearUser();
+        } else {
+            context.setUser(user);
+        }
+
+        context.success();
+    }
+
+    @Override
+    public boolean requiresUser() {
+        return false;
+    }
+
+    @Override
+    public boolean configuredFor(KeycloakSession session, RealmModel realm, UserModel user) {
+        return true;
+    }
+
+    @Override
+    public void setRequiredActions(KeycloakSession session, RealmModel realm, UserModel user) {
+
+    }
+
+    @Override
+    public String getDisplayType() {
+        return "SPI- Choose User";
+    }
+
+    @Override
+    public String getReferenceCategory() {
+        return null;
+    }
+
+    @Override
+    public boolean isConfigurable() {
+        return false;
+    }
+
+    public static final AuthenticationExecutionModel.Requirement[] REQUIREMENT_CHOICES = {
+            AuthenticationExecutionModel.Requirement.REQUIRED
+    };
+
+    @Override
+    public AuthenticationExecutionModel.Requirement[] getRequirementChoices() {
+        return REQUIREMENT_CHOICES;
+    }
+
+    @Override
+    public boolean isUserSetupAllowed() {
+        return false;
+    }
+
+    @Override
+    public String getHelpText() {
+        return "Choose a user to reset credentials for";
+    }
+
+    @Override
+    public List<ProviderConfigProperty> getConfigProperties() {
+        return null;
+    }
+
+    @Override
+    public void close() {
+
+    }
+
+    @Override
+    public Authenticator create(KeycloakSession session) {
+        return this;
+    }
+
+    @Override
+    public void init(Config.Scope config) {
+
+    }
+
+    @Override
+    public void postInit(KeycloakSessionFactory factory) {
+
+    }
+
+    @Override
+    public String getId() {
+        return PROVIDER_ID;
+    }
+}
+
